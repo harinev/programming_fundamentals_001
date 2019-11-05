5f49e7bb581a4d3d40622c35bef2a29bf4484db1
harine <harinev@outlook.com>
2019-11-05 16:10:05 +0000

    Updated bookshop utils

--- app/bookshop_utils.js
+++ app/bookshop_utils.js
@@ -1,7 +1,20 @@
 function addVAT(price) {
     return price + (0.2 * price);
 }
+function getFullName (firstName, lastName){
+    return firstName +" "+ lastName
+}
+function makeHalfPrice(price) {
+    var disc= price/2;
+    return disc
+    }
+function countBooks(arr){
+    return arr.length;
+    }
 
 module.exports = {
-    addVAT
+    addVAT,
+    getFullName,
+    makeHalfPrice,
+    countBooks
 };
--- test/utils.test.js
+++ test/utils.test.js
@@ -4,8 +4,34 @@ describe("utils.addVAT", () => {
   test("returns 36 when passed 30", () => {
     expect(utils.addVAT(30)).toBe(36);
   });
+  test("returns 118.8 when passed 99", () => {
+    expect(utils.addVAT(99)).toBe(118.8);
+  });
   
   test("returns 120 when passed 100", () => {
     expect(utils.addVAT(100)).toBe(120);
   });
+  
 });
+describe("utils.getFullName", () => {
+  test("returns Haruki Murakami when passed Haruki and Murakami", () => {
+    expect(utils.getFullName("Haruki", "Murakami")).toBe("Haruki Murakami");
+  });
+});
+describe("utils.makeHalfPrice", () => {
+  test("returns 100 when passed 200", () => {
+    expect(utils.makeHalfPrice(200)).toBe(100);
+  });
+});
+describe("utils.countBooks", () => {
+  test("returns 5 when passed the list", () => {
+    expect(utils.countBooks(["book1", "b2", "b3", "b4", "b5"])).toBe(5);
+  });
+ });
+
+// {
+//   title: "The Stone Diaries",
+//   author: "Carol Shields",
+//   yearOfPublication: 1993,
+//   quantity: 9
+// }
